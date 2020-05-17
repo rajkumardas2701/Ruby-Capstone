ad0e214cd00efd06a93cb3deec1f30d320b278b4
rajkumardas2701 <41339878+rajkumardas2701@users.noreply.github.com>
2020-05-18 04:25:54 +0530

    adding Festival class to pull festivals of current month

--- bin/main.rb
+++ bin/main.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/env ruby
 require_relative '../lib/corona'
+require_relative '../lib/festivals'
 
 corona = Corona.new
 puts 'Status of Corona Cases in India ' + corona.date
@@ -7,3 +8,6 @@ puts 'Active Case: ' + corona.active
 puts 'Discharge Case: ' + corona.discharge
 puts 'Death Case: ' + corona.death
 puts 'Migrated Case: ' + corona.migrated
+
+fest = Festival.new
+fest.print_festivals
--- lib/festivals.rb
+++ lib/festivals.rb
@@ -3,9 +3,43 @@ require 'httparty'
 require 'byebug'
 
 class Festival
+  # attr_reader :print_festivals
+
   def initialize
-    puts Time.now
+    url = 'https://www.drikpanchang.com/calendars/indian/indiancalendar.html'
+    cal_unparsed = HTTParty.get(url)
+    cal_parsed = Nokogiri::HTML(cal_unparsed)
+    @events = cal_parsed.css('div.dpEventInfo')
+    @time_today = Time.now.to_s
+    @months = %w[January February
+                 March April May June July August September October
+                 November December]
+  end
+
+  private
+
+  def festivals
+    all_events = {}
+    @events.each do |event|
+      event_date = event.css('div.dpEventGregDate').text
+      all_events[event_date.to_s] = event.css('div.dpEventName').text
+    end
+    all_events
+  end
+
+  def current_month
+    h = @time_today.split(' ')
+    month = h[0].split('-')
+    l = month[1].to_i
+    @months[l - 1].capitalize
+  end
+
+  public
+
+  def print_festivals
+    puts "Festivals in the month of #{current_month} are:"
+    festivals.each do |date, event|
+      puts "#{date}, celebrated as \"#{event}\"" if date.include? current_month
+    end
   end
 end
-
-# time = Festival.new
