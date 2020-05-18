c62a04fdb47c481fcabefcec561bcf2f32fa6132
rajkumardas2701 <41339878+rajkumardas2701@users.noreply.github.com>
2020-05-18 21:59:00 +0530

    adding jobs class which will give update on latest vacancies

--- bin/main.rb
+++ bin/main.rb
@@ -2,19 +2,20 @@
 require_relative '../lib/corona'
 require_relative '../lib/festivals'
 require_relative '../lib/twitter_connect'
+require_relative '../lib/jobs'
 
-puts 'Please enter below details to connect with your twitter Bot'
-puts 'Enter your consumer key:'
-con_key = gets.chomp
-puts 'Enter your consumer secret:'
-con_sec = gets.chomp
-puts 'Enter your access token:'
-acc_token = gets.chomp
-puts 'Enter your access token secret:'
-acc_secret = gets.chomp
+# puts 'Please enter below details to connect with your twitter Bot'
+# puts 'Enter your consumer key:'
+# con_key = gets.chomp
+# puts 'Enter your consumer secret:'
+# con_sec = gets.chomp
+# puts 'Enter your access token:'
+# acc_token = gets.chomp
+# puts 'Enter your access token secret:'
+# acc_secret = gets.chomp
 
 # twitter_con =
-TwitterConnect.new(con_key, con_sec, acc_token, acc_secret)
+# TwitterConnect.new(con_key, con_sec, acc_token, acc_secret)
 
 # corona = Corona.new
 # puts 'Status of Corona Cases in India ' + corona.date
@@ -25,3 +26,6 @@ TwitterConnect.new(con_key, con_sec, acc_token, acc_secret)
 
 # fest = Festival.new
 # fest.print_festivals
+
+job = Jobs.new
+p job.print_jobs
--- lib/jobs.rb
+++ lib/jobs.rb
@@ -3,5 +3,40 @@ require 'httparty'
 require 'byebug'
 
 class Jobs
-  def initialize; end
+  def initialize
+    url = 'https://www.freshersworld.com/jobs-in-bangalore/9999016065'
+    jobs_unparsed = HTTParty.get(url)
+    jobs_parsed = Nokogiri::HTML(jobs_unparsed)
+    @jobs = jobs_parsed.css('div.job-container')
+    @vacancies = []
+    # @value = @jobs.css('div.job-desc-block')
+
+    # @value1 = @value[0].css('div')[1]
+    # @value2 = @value1.css('div')[1]
+    # p @title = @value2.text
+  end
+
+  def fetch_jobs
+    @jobs.each do |job|
+      value = job.css('div.job-desc-block')
+      value1 = value[0].css('div')[1]
+      value2 = value1.css('div')[1]
+      # puts "running loop #{i}th times"
+      @job_posting = {
+        company: job.css('h3.latest-jobs-title').text,
+        position: value2.text,
+        apply_url: job.css('a')[0].attributes['href'].value
+      }
+
+      @vacancies << @job_posting
+    end
+  end
+
+  def print_jobs
+    fetch_jobs
+    @vacancies
+  end
 end
+
+# job = Jobs.new
+# p job.print_jobs
