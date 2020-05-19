425442e44af13b65b0825f65547ad70d316cfb55
rajkumardas2701 <41339878+rajkumardas2701@users.noreply.github.com>
2020-05-19 05:56:52 +0530

    added logic to post covid status to Twitter

--- bin/main.rb
+++ bin/main.rb
@@ -14,18 +14,26 @@ require_relative '../lib/jobs'
 # puts 'Enter your access token secret:'
 # acc_secret = gets.chomp
 
-# twitter_con =
-# TwitterConnect.new(con_key, con_sec, acc_token, acc_secret)
-
+# twitter_con = TwitterConnect.new
+# (con_key, con_sec, acc_token, acc_secret)
 # corona = Corona.new
+fest = Festival.new
+# job = Jobs.new
+
 # puts 'Status of Corona Cases in India ' + corona.date
 # puts 'Active Case: ' + corona.active
 # puts 'Discharged Case: ' + corona.discharge
 # puts 'Death Case: ' + corona.death
 # puts 'Migrated Case: ' + corona.migrated
 
-# fest = Festival.new
-# fest.print_festivals
+fest.print_festivals
 
-job = Jobs.new
-p job.print_jobs
+# job.print_jobs
+
+# loop do
+#   twitter_con.tweet("Status of Corona Cases in India #{corona.date}
+#   Active Case: #{corona.active}
+#   Discharged Case: #{corona.discharge}
+#   Death Case: #{corona.death}
+#   Migrated Case: #{corona.migrated}", 10)
+# end
